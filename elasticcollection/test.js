71862502e9a2dc5ee43b0e74d9d63a9c16afd811
Viviinn <vivian.yunqi.chen@gmail.com>
2023-03-24 16:42:39 -0400

    add
    elastic collection

---
+++ elasticcollection/test.js
@@ -0,0 +1,55 @@
+const data = {
+    "times": [
+        "6:44 PM",
+        "12:56 PM",
+        "3:10 PM",
+        "1:13 PM",
+        "2:01 PM",
+        "2:47 PM",
+        "4:35 PM",
+        "5:59 PM",
+        "1:01 PM",
+        "6:38 PM",
+        "5:05 PM",
+        "5:12 PM",
+        "6:13 PM",
+        "5:08 PM",
+        "10:11 PM",
+        "5:11 PM",
+        "6:04 PM",
+        "4:23 PM",
+        "7:02 PM",
+        "7:05 PM",
+        "10:12 PM",
+        "10:19 AM",
+        "4:47 PM",
+        "9:43 PM",
+        "4:35 PM"
+    ]
+  };
+  
+  document.getElementById("time1").innerHTML = data.times[0];
+  document.getElementById("time2").innerHTML = data.times[1];
+  document.getElementById("time3").innerHTML = data.times[2];
+  document.getElementById("time4").innerHTML = data.times[3];
+  document.getElementById("time5").innerHTML = data.times[4];
+  document.getElementById("time6").innerHTML = data.times[5];
+  document.getElementById("time7").innerHTML = data.times[6];
+  document.getElementById("time8").innerHTML = data.times[7];
+  document.getElementById("time9").innerHTML = data.times[8];
+  document.getElementById("time10").innerHTML = data.times[9];
+  document.getElementById("time11").innerHTML = data.times[10];
+  document.getElementById("time12").innerHTML = data.times[11];
+  document.getElementById("time13").innerHTML = data.times[12];
+  document.getElementById("time14").innerHTML = data.times[13];
+  document.getElementById("time15").innerHTML = data.times[14];
+  document.getElementById("time16").innerHTML = data.times[15];
+  document.getElementById("time17").innerHTML = data.times[16];
+  document.getElementById("time18").innerHTML = data.times[17];
+  document.getElementById("time19").innerHTML = data.times[18];
+  document.getElementById("time20").innerHTML = data.times[19];
+  document.getElementById("time21").innerHTML = data.times[20];
+  document.getElementById("time22").innerHTML = data.times[21];
+  document.getElementById("time23").innerHTML = data.times[22];
+  document.getElementById("time24").innerHTML = data.times[23];
+  document.getElementById("time25").innerHTML = data.times[24];
